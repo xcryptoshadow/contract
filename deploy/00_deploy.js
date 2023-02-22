9f590668b917cc8815d0d896a944e73d899ce2d9
sagarjethi <sagar.jethi007@gmail.com>
2023-02-23 04:02:28 +0530

    update contracts

--- deploy/00_deploy.js
+++ deploy/00_deploy.js
@@ -10,26 +10,12 @@ const wallet = new ethers.Wallet(private_key, ethers.provider)
 module.exports = async ({ deployments }) => {
     console.log("Wallet Ethereum Address:", wallet.address)
     const chainId = network.config.chainId
-    const tokensToBeMinted = networkConfig[chainId]["tokensToBeMinted"]
 
-    //deploy Simplecoin
-    const SimpleCoin = await ethers.getContractFactory('SimpleCoin', wallet);
-    console.log('Deploying Simplecoin...');
-    const simpleCoin = await SimpleCoin.deploy(tokensToBeMinted);
-    await simpleCoin.deployed()
-    console.log('SimpleCoin deployed to:', simpleCoin.address);
+    //deploy multiSigFactory
+    const MultiSigFactory = await ethers.getContractFactory('MultiSigFactory');
+    console.log('Deploying MultiSigFactory...');
+    const multiSigFactory = await MultiSigFactory.deploy();
+    await multiSigFactory.deployed()
+    console.log('multiSigFactory deployed to:', multiSigFactory.address);
 
-    //deploy FilecoinMarketConsumer
-    const FilecoinMarketConsumer = await ethers.getContractFactory('FilecoinMarketConsumer', wallet);
-    console.log('Deploying FilecoinMarketConsumer...');
-    const filecoinMarketConsumer = await FilecoinMarketConsumer.deploy();
-    await filecoinMarketConsumer.deployed()
-    console.log('FilecoinMarketConsumer deployed to:', filecoinMarketConsumer.address);
-
-    //deploy DealRewarder
-    const DealRewarder = await ethers.getContractFactory('DealRewarder', wallet);
-    console.log('Deploying DealRewarder...');
-    const dealRewarder = await DealRewarder.deploy();
-    await dealRewarder.deployed()
-    console.log('DealRewarder deployed to:', dealRewarder.address);
 }
